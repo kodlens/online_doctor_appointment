9f501a3984279cf9aafbbf083c6f0e07e2096239
Etienne Wayne <etiennewayne@gmail.com>
2023-05-04 10:11:35 +0800

    udpate repo

---
+++ database/seeders/AppointmentSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AppointmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+
+        $data = [
+            [
+                'schedule_id' => 2,
+                'user_id' => 2,
+                'appointment_date' => '2023-05-04',
+                'illness_history' => 'sakit tiyan',
+                'status' => 0,
+
+            ],
+            [
+                'schedule_id' => 2,
+                'user_id' => 2,
+                'appointment_date' => '2023-05-09',
+                'illness_history' => 'sakit tiyan',
+                'status' => 0,
+
+            ],
+            [
+                'schedule_id' => 2,
+                'user_id' => 2,
+                'appointment_date' => '2023-05-08',
+                'illness_history' => 'sakit tiyan',
+                'status' => 0,
+
+            ],
+
+
+            [
+                'schedule_id' => 2,
+                'user_id' => 2,
+                'appointment_date' => '2023-05-08',
+                'illness_history' => 'sakit tiyan',
+                'status' => 2,
+
+            ],
+
+        ];
+
+        \App\Models\Appointment::insertOrIgnore($data);
+
+    }
+}
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -33,9 +33,9 @@ class UserSeeder extends Seeder
             ],
 
             [
-                'username' => 'user01',
-                'lname' => 'DELA CRUZ',
-                'fname' => 'JUAN',
+                'username' => 'vinz',
+                'lname' => 'SENTIMENT',
+                'fname' => 'VINCENT',
                 'mname' => 'P',
                 'sex' => 'MALE',
                 'province' => '1042',
@@ -47,6 +47,84 @@ class UserSeeder extends Seeder
                 'role' => 'USER',
                 'password' => Hash::make('a')
             ],
+
+            [
+                'username' => 'joel',
+                'lname' => 'LACAPAG',
+                'fname' => 'JOEL',
+                'mname' => 'P',
+                'sex' => 'MALE',
+                'province' => '1042',
+                'city' => '104215',
+                'barangay' => '104215027',
+                'street' => 'P-6',
+                //'email' => 'doctor@dev.com',
+                'contact_no' => '09216548722',
+                'role' => 'USER',
+                'password' => Hash::make('a')
+            ],
+
+            [
+                'username' => 'lensey',
+                'lname' => 'ONG',
+                'fname' => 'LENSEY',
+                'mname' => 'B',
+                'sex' => 'FEMALE',
+                'province' => '1042',
+                'city' => '104215',
+                'barangay' => '104215027',
+                'street' => 'P-6',
+                //'email' => 'doctor@dev.com',
+                'contact_no' => '09216548123',
+                'role' => 'USER',
+                'password' => Hash::make('a')
+            ],
+            [
+                'username' => 'richalle',
+                'lname' => 'PLAZA',
+                'fname' => 'RICHALLE MAE',
+                'mname' => 'L',
+                'sex' => 'FEMALE',
+                'province' => '1042',
+                'city' => '104215',
+                'barangay' => '104215027',
+                'street' => 'P-6',
+                //'email' => 'doctor@dev.com',
+                'contact_no' => '09216548223',
+                'role' => 'USER',
+                'password' => Hash::make('a')
+            ],
+            [
+                'username' => 'jessa',
+                'lname' => 'SARAGINA',
+                'fname' => 'JESSA MAE',
+                'mname' => 'M',
+                'sex' => 'FEMALE',
+                'province' => '1042',
+                'city' => '104215',
+                'barangay' => '104215027',
+                'street' => 'P-6',
+                //'email' => 'doctor@dev.com',
+                'contact_no' => '09216548441',
+                'role' => 'USER',
+                'password' => Hash::make('a')
+            ],
+            [
+                'username' => 'john',
+                'lname' => 'BARINAN',
+                'fname' => 'JOHN RIVIN',
+                'mname' => 'O',
+                'sex' => 'MALE',
+                'province' => '1042',
+                'city' => '104215',
+                'barangay' => '104215027',
+                'street' => 'P-6',
+                //'email' => 'doctor@dev.com',
+                'contact_no' => '09216548444',
+                'role' => 'USER',
+                'password' => Hash::make('a')
+            ],
+
             
             
         ];
